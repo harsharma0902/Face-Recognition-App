64d8c00672b814d76b073406633230ab2a6d55f8
Harsh Sharma <harsharma0902@gmail.com>
2022-03-20 09:12:39 +0530

    Added code files via upload

---
+++ src/App.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+import { render } from '@testing-library/react';
+
+// const app = new Clarifai.App({
+//   apiKey: 'bac7015f67e84ac08f40cb719434bffd'
+// });
+
+const raw = JSON.stringify({
+  "user_app_id": {
+    "user_id": "harsharma0902",
+    "app_id": "c2ee5dd653a0480786247722499234ac"
+  },
+  "inputs": [
+    {
+      "data": {
+        "image": {
+          "url": "https://samples.clarifai.com/metro-north.jpg"
+        }
+      }
+    }
+  ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Key bac7015f67e84ac08f40cb719434bffd'
+  },
+  body: raw
+};
+
+const particlesInit = (main) => {
+  console.log(main);
+
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+};
+
+const particlesLoaded = (container) => {
+  console.log(container);
+};
+
+const particlesOptions = {
+  background: {
+    color: {
+      // value: "#0d47a1",
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 18,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#FBE5E5",
+    },
+    links: {
+      color: "#FBE5E5",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.2,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height, clarifaiFace);
+    return {
+      leftCol: width - (clarifaiFace.left_col * width),
+      topRow: (clarifaiFace.top_row * height) / 2,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageURL: this.state.input });
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", requestOptions)
+      .then(response => response.text())
+      // .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data.regions[0].region_info.bounding_box))
+      .then(result => this.displayFaceBox(this.calculateFaceLocation(JSON.parse(result, null, 2))))
+      .catch(error => console.log('error', error));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    if (route === 'signout') {
+      this.setState({ route: 'signin' });
+    } else {
+      this.setState({ route: route });
+    }
+  }
+
+  render() {
+    const { isSignedIn, imageURL, route, box } = this.state;
+    return (
+      <div className="App">
+        <Particles className="particles"
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home'
+          ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={box} imageURL={imageURL} />
+          </div>
+          :
+          (route === 'signin')
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />}
+      </div>
+    );
+  }
+}
+
+export default App;
